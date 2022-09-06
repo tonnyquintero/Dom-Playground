de296989327fa629b230f4783167f9c864c7d095
tonny187 <tonyoanthony@gmail.com>
2022-09-06 17:35:28 -0500

    Dom Playground

---
+++ main.js
@@ -0,0 +1,15 @@
+const H1 = document.querySelector('h1')
+const p = document.querySelector('p')
+const parrafito = document.querySelector('.parrafito')
+const pid = document.querySelector('#pid')
+const input = document.querySelector('input')
+
+console.log({
+    H1,
+    p,
+    parrafito,
+    pid,
+    input,
+})
+
+console.log(input.value)
